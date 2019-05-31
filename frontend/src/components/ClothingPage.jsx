f1790c30bcbd9e4dc7f84a0651b6c153362ca2d4
Sabine Robart <sabine.robart@protonmail.com>
2019-05-31 17:18:51 +0200

    [Add] clothing page component (basic layout).

---
+++ frontend/src/components/ClothingPage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../style/ClothingPage.scss";
+// import Profile from "./Profile";
+
+class ClothingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentsNumber: 35
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Container className="clothing-container">
+          <Row>
+            <Col lg="7" />
+            <Col lg="5" className="comments-container border text-center">
+              <div className="sub-container">
+                {/* <Profile /> */}
+                <div className="fake-profile">Fake Profile</div>
+                <h2>{this.state.commentsNumber} Commentaires</h2>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ClothingPage;
