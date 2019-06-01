d00f03b7990d39c31f7ea418f8366f2545670ae0
Sabine Robart <sabine.robart@protonmail.com>
2019-06-01 12:42:11 +0200

    [Add] import Comment component into the Clothing page (waiting for data to map them).

--- frontend/src/components/ClothingPage.jsx
+++ frontend/src/components/ClothingPage.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "../style/ClothingPage.scss";
 // import Profile from "./Profile";
+import Comment from "./Comment";
 
 class ClothingPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      commentsNumber: 35
+      commentsNumber: 35,
+      avatar: "https://randomuser.me/api/portraits/women/90.jpg",
+      username: "Jade",
+      comment: "Super joli! Lorem ipsum dolor sit amet consectblablabla"
     };
   }
+
   render() {
     return (
       <React.Fragment>
@@ -21,7 +26,19 @@ class ClothingPage extends React.Component {
                 {/* <Profile /> */}
                 <div className="fake-profile">Fake Profile</div>
                 <h2>{this.state.commentsNumber} Commentaires</h2>
-                <div className="comments-feed" />
+                <div className="comments-feed">
+                  {/*map comments here */}
+                  <Comment
+                    avatar={this.state.avatar}
+                    username={this.state.username}
+                    comment={this.state.comment}
+                  />
+                  <Comment
+                    avatar={this.state.avatar}
+                    username={this.state.username}
+                    comment={this.state.comment}
+                  />
+                </div>
               </div>
             </Col>
           </Row>
