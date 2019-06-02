00a270ae382e4eef451d4485f2efa546c28a306f
Sabine Robart <sabine.robart@protonmail.com>
2019-06-02 18:56:24 +0200

    [Add] basic profile layout.

--- frontend/src/components/ClothingPage.jsx
+++ frontend/src/components/ClothingPage.jsx
@@ -27,7 +27,31 @@ class ClothingPage extends React.Component {
             <Col lg="5" className="comments-container text-center">
               <div className="sub-container">
                 {/* <Profile /> */}
-                <div className="fake-profile">Fake Profile</div>
+                <div className="p-4 comments-feed">
+                  <div className="m-3">
+                    <Row className="align-items-center">
+                      <Col xs="4">
+                        <img
+                          src={this.state.profileInfo.avatar}
+                          alt={`user-${this.state.profileInfo.id}`}
+                          className="avatar"
+                        />
+                      </Col>
+                      <Col xs="8" className="text-left">
+                        {this.state.profileInfo.username}
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className="text-left">
+                    Petite tenue pour cet hiver. Jupe et pull dispo. Les
+                    collants: du basique noir.
+                  </div>
+                  <ul className="text-left list-unstyled list-inline">
+                    <li className="list-inline-item">#jupe</li>
+                    <li className="list-inline-item">#casual</li>
+                    <li className="list-inline-item">#winteriscoming</li>
+                  </ul>
+                </div>
                 <h2>{this.state.commentsNumber} Commentaires</h2>
                 <div className="comments-feed">
                   {/*map comments here */}
