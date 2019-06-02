5b3e1e0bb885dd728e4dd19e582167304838585f
Sabine Robart <sabine.robart@protonmail.com>
2019-06-02 18:26:44 +0200

    [refa] store props in a variable.

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -27,19 +27,16 @@ class Comment extends React.Component {
   }
 
   render() {
+    const info = this.props.info;
     return (
       <React.Fragment>
         <Row className="comment-line mx-1 my-2">
           <Col xs="2">
-            <img
-              src={this.props.avatar}
-              alt="username-goes-here" // !
-              className="avatar"
-            />
+            <img src={info.avatar} alt={`user-${info.id}`} className="avatar" />
           </Col>
-          <Col xs="2">{this.props.username}</Col>
+          <Col xs="2">{info.username}</Col>
           <Col xs="7" className="text-left">
-            {this.props.comment}
+            {info.comment}
           </Col>
           <Col xs="1" className="p-0">
             <ButtonDropdown
