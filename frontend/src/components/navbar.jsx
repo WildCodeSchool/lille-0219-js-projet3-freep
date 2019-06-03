2716455c3dc12c119a81c5fa9e148c29d396c515
Louis Allard <l.allard@yahoo.fr>
2019-06-03 10:32:13 +0200

    create style and components directory AND navbar.css and NavBar component.

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, {Fragment}  from 'react';
+import '../style/navbar.css';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <Fragment>
+          <div className="navbar">
+            <div>   
+                
+            </div>
+          </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Navbar;
