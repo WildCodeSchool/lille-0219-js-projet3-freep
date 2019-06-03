129cc0c5e239cb29c9858f288dcc1b7c77695930
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-03 17:03:18 +0200

    create a photo component

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Heart, Target } from "react-feather";
 
 class Photo extends React.Component {
-    render() {
-        return()
-    }
-};
+  render() {
+    return (
+      <React.Fragment>
+        <img src="" alt="clothes' picture" />
+        <Heart />
+        <Target />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Photo;
