93749169ce32e9e5067ecdc0e361b5d8c7ec5915
Louis Allard <l.allard@yahoo.fr>
2019-06-04 17:58:30 +0200

    Add boostrap css into index.js.

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,10 +6,12 @@ import {
   Navbar,
   NavLink,
   Nav,
+  Form,
+  Container, Row, Col
 } from "reactstrap";
-import {  BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Routing from "./routing";
-import {  Plus, Mail, Heart, User } from "react-feather";
+import { Plus, Mail, Heart, User } from "react-feather";
 import "../style/navbar.css";
 
 class NavFreep extends React.Component {
@@ -17,47 +19,58 @@ class NavFreep extends React.Component {
     return (
       <Fragment>
         <Router>
-          <div className="header">
-            <Fragment>
-              <div className="menu">
-                <Navbar color="black" light expand="md">
-                  <img
-                    className="logo"
-                    src="https://via.placeholder.com/100"
-                    alt="logo"
-                  />
-                </Navbar>
-                <div className="title">
-                  <span>Freep</span>
-                  <br />
-                  <span>La garde robe qui rapporte</span>
-                </div>
-                  <form className="recherche">
+          <Container>
+            <Row>
+              <div className="header">
+                <Col xs="2">
+                  <Navbar color="black" light expand="md">
+                    <img
+                      className="logo"
+                      src="https://via.placeholder.com/100"
+                      alt="logo"
+                    />
+                  </Navbar>
+                </Col>
+                <Col xs="3">
+                  <div className="title">
+                    <span>Freep</span>
+                    <br />
+                    <span>La garde robe qui rapporte</span>
+                  </div>
+                </Col>
+                <Col xs="4">
+                  <Form className="recherche">
                     <InputGroup>
                       <InputGroupAddon addonType="prepend" />
-                      <Input className="search" placeholder="Rechercher un vêtement ou une cliente" />
+                      <Input
+                        className="search"
+                        placeholder="Rechercher un vêtement ou une cliente"
+                      />
                     </InputGroup>
-                  </form>
+                  </Form>
+                </Col>
+                <Col xs="4">
                   <div className="picto">
-                <Nav className="d-flex ml-auto" horizontal="end" navbar>
-                  <NavLink href="#">
-                    <Plus className="img" color="black" />
-                  </NavLink>
-                  <NavLink href="#">
-                    <Mail className="img" color="black" />
-                  </NavLink>
-                  <NavLink href="#">
-                    <Heart className="img" color="black" />
-                  </NavLink>
-                  <NavLink href="#">
-                    <User className="img" color="black" />
-                  </NavLink>
-                </Nav>
-                </div>
+                    <Nav className="d-flex ml-auto" horizontal="end" navbar>
+                      <NavLink href="#">
+                        <Plus className="img" color="black" />
+                      </NavLink>
+                      <NavLink href="#">
+                        <Mail className="img" color="black" />
+                      </NavLink>
+                      <NavLink href="#">
+                        <Heart className="img" color="black" />
+                      </NavLink>
+                      <NavLink href="#">
+                        <User className="img" color="black" />
+                      </NavLink>
+                    </Nav>
+                  </div>
+                </Col>
+                <Routing />
               </div>
-            </Fragment>
-            <Routing />
-          </div>
+            </Row>
+          </Container>
         </Router>
         <hr />
       </Fragment>
