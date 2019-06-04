d066eb75ba39354536fc3f2fb95c2fabe491b70c
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-04 10:10:34 +0200

    create a placeholder for the photo

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Heart, Target, MoreHorizontal } from "react-feather";
+import { Row, Col } from "reactstrap";
 
 class Photo extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <img src="" alt="clothes' picture" />
-        <Heart />
-        <Target />
-        <MoreHorizontal />
+        <img src="https://via.placeholder.com/150" alt="clothes' picture" />
+        <Row>
+          <Heart />
+          <Target />
+          <MoreHorizontal />
+        </Row>
       </React.Fragment>
     );
   }
