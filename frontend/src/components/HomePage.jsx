d7a409af80627c11d62d07eb857cda855bf41b17
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-04 10:18:22 +0200

    Create a homepage component

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+import Photo from "./Photo";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Row>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+          <Col className="col-5">
+            <Photo />
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomePage;
