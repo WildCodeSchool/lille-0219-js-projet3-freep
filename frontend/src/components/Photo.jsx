01e7723351e4c1e27395d2cc2add9fd97806c5ad
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-04 14:16:02 +0200

    import react router in the component Photo.jsx

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Heart, Target, MoreHorizontal } from "react-feather";
 import { Row } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Photo extends React.Component {
   render() {
     return (
       <div>
-        <img src="https://via.placeholder.com/150" alt="clothes" />
+        <Link to="/ClothingPage">
+          <img src="https://via.placeholder.com/150" alt="clothes" />
+        </Link>
         <Row>
           <Heart />
           <Target />
