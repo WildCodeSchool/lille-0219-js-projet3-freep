259a421cc6a1874f6dd7d31519974e62956fc844
Richard Gaillet <gaillet.richard@gmail.com>
2019-06-04 17:11:01 +0200

    Import Container from Reactstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Profile from "./components/Profile";
+import { Container } from 'reactstrap';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Profile />
-      </header>
-    </div>
+    <Container>
+      <Profile />
+    </Container>
   );
 }
 
