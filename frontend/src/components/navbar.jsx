dd6ecfe06e2d379cb1bb22f70b1d3b75049d5f34
Louis Allard <l.allard@yahoo.fr>
2019-06-06 17:44:32 +0200

    Add Route.

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,59 +7,55 @@ import {
   NavLink,
   Nav,
   Form,
-NavbarToggler,
-Collapse
+  NavbarToggler,
+  Collapse
 } from "reactstrap";
-import { BrowserRouter as Router } from "react-router-dom";
-import Routing from "./routing";
 import { Plus, Mail, Heart, User } from "react-feather";
 import "../style/navbar.css";
-
-
 
 class NavFreep extends React.Component {
   render() {
     return (
-        <div className="header">
+      <div className="header">
         <Navbar color="light" light expand="md">
-        <Navbar color="black" light>
-                    <img
-                      className="logo"
-                      src="https://via.placeholder.com/100"
-                      alt="logo"
-                    />
-                  </Navbar>
-                  <div className="title">
-                    <span>Freep</span>
-                    <br />
-                    <span>La garde robe qui rapporte</span>
-                  </div>
+          <Navbar color="black" light>
+            <Link to="/">
+              <img
+                className="logo"
+                src="https://via.placeholder.com/100"
+                alt="logo"
+              />
+            </Link>
+          </Navbar>
+          <div className="title">
+            <span>Freep</span>
+            <br />
+            <span>La garde robe qui rapporte</span>
+          </div>
           <NavbarToggler onClick={this.toggle} />
           <Form className="recherche">
-                    <InputGroup>
-                      <InputGroupAddon addonType="prepend" />
-                      <Input
-                        className="search"
-                        placeholder="Rechercher un vêtement ou une cliente"
-                      />
-                    </InputGroup>
-                  </Form>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend" />
+              <Input
+                className="search"
+                placeholder="Rechercher un vêtement ou une cliente"
+              />
+            </InputGroup>
+          </Form>
           <Collapse navbar>
             <Nav className="ml-auto" navbar>
-
-                      <NavLink href="#">
-                        <Plus className="img" color="black" />
-                      </NavLink>
-                      <NavLink href="#">
-                        <Mail className="img" color="black" />
-                      </NavLink>
-                      <NavLink href="#">
-                        <Heart className="img" color="black" />
-                      </NavLink>
-                      <NavLink href="#">
-                        <User className="img" color="black" />
-                      </NavLink>
-                    
+              <NavLink to="/">
+                <Plus className="img" color="black" />
+              </NavLink>
+              <NavLink top="/messagerie">
+                <Mail className="img" color="black" />
+              </NavLink>
+              <NavLink to="/favoris">
+                <Heart className="img" color="black" />
+              </NavLink>
+              <NavLink to="/profil">
+                <User className="img" color="black" />
+              </NavLink>
             </Nav>
           </Collapse>
         </Navbar>
