c042be5e77ba92eae03560526e57af41d3ee54ac
Sabine Robart <sabine.robart@protonmail.com>
2019-06-06 17:57:11 +0200

    [Add] navigation routing.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import ClothingPage from "./components/ClothingPage";
+import Profile from "./components/Profile";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/article" component={ClothingPage} />
+      <Route path="/article/:articleId" component={ClothingPage} />
+      <Route path="/profil" component={Profile} />
+      <Route path="/profil/:profilId" component={Profile} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
