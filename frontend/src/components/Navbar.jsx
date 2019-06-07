787472b41d1cc37ee41b6098315d9bc07ae8f0c3
Louis Allard <l.allard@yahoo.fr>
2019-06-07 14:18:25 +0200

    ADD LINK COMPONENT and CHANGE CASE on file name.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Navbar,
+  NavLink,
+  Nav,
+  Form,
+  NavbarToggler,
+  Collapse
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Plus, Mail, Heart, User } from "react-feather";
+import "../style/navbar.css";
+
+class NavFreep extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <Navbar color="light" light expand="md">
+          <Navbar color="black" light>
+            <Link to="/">
+              <img
+                className="logo"
+                src="https://via.placeholder.com/100"
+                alt="logo"
+              />
+            </Link>
+          </Navbar>
+          <div className="title">
+            <span>Freep</span>
+            <br />
+            <span>La garde robe qui rapporte</span>
+          </div>
+          <NavbarToggler onClick={this.toggle} />
+          <Form className="search-input">
+            <InputGroup>
+              <InputGroupAddon addonType="prepend" />
+              <Input
+                className="search"
+                placeholder="Rechercher un vêtement ou une cliente"
+              />
+            </InputGroup>
+          </Form>
+          <Collapse navbar>
+            <Nav className="ml-auto" navbar>
+              <NavLink to="/">
+                <Plus className="img" color="black" />
+              </NavLink>
+              <NavLink top="/messagerie">
+                <Mail className="img" color="black" />
+              </NavLink>
+              <NavLink to="/favoris">
+                <Heart className="img" color="black" />
+              </NavLink>
+              <NavLink to="/profil">
+                <User className="img" color="black" />
+              </NavLink>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default NavFreep;
