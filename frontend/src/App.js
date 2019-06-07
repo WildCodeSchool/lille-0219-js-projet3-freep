6c584b9536e9c67bc330a487a7bad025280fd9be
Sabine Robart <sabine.robart@protonmail.com>
2019-06-07 12:40:00 +0200

    [refa] move the navigation routes to the App component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import Profile from "./components/Profile";
-import { Container } from 'reactstrap';
+import { Container } from "reactstrap";
+import { Route, Switch } from "react-router-dom";
+import ClothingPage from "./components/ClothingPage";
 import "./App.css";
 
 function App() {
   return (
     <Container>
-      <HomePage />
-      <Profile />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/article" component={ClothingPage} />
+        <Route path="/article/:articleId" component={ClothingPage} />
+        <Route path="/profil" component={Profile} />
+        <Route path="/profil/:profilId" component={Profile} />
+      </Switch>
     </Container>
   );
 }
