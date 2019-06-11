5613cd85e35040cc6fe42d89dde21a61e40e0804
Sabine Robart <sabine.robart@protonmail.com>
2019-06-11 17:29:46 +0200

    [refa] import infos from database into the clothing page.

--- frontend/src/components/ClothingPage.jsx
+++ frontend/src/components/ClothingPage.jsx
@@ -9,25 +9,19 @@ class ClothingPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // commentsNumber: 35,
-      profileInfo: [
-        // {
-        //   id: 1,
-        //   id_user: 1,
-        //   type: "Veste",
-        //   brand: "ZARA",
-        //   size: "XXXL",
-        //   gender: "F",
-        //   description: "Veste Femme XXXL",
-        //   is_deposit: 1,
-        //   created_at: "2019-05-14T22:00:00.000Z"
-        // }
-      ]
+      profileInfo: [],
+      clothingInfo: []
     };
   }
 
   componentDidMount() {
     axios.get(`http://localhost:5050/articles`).then(({ data }) => {
+      this.setState({
+        clothingInfo: data[0]
+      });
+    });
+
+    axios.get(`http://localhost:5050/users`).then(({ data }) => {
       this.setState({
         profileInfo: data[0]
       });
@@ -35,7 +29,8 @@ class ClothingPage extends React.Component {
   }
 
   render() {
-    console.log(this.state.profileInfo);
+    const profile = this.state.profileInfo;
+    const clothing = this.state.clothingInfo;
     return (
       <React.Fragment>
         <Container className="clothing-container">
@@ -59,33 +54,28 @@ class ClothingPage extends React.Component {
                   <div className="m-3">
                     <Row className="align-items-center">
                       <Col xs="4">
-                        {/* <Profile /> */}
                         <img
-                          // src={this.state.profileInfo.avatar}
-                          // alt={`user-${this.state.profileInfo.id}`}
+                          src={profile.avatar}
+                          alt={`user-${profile.id}`}
                           className="avatar"
                         />
                       </Col>
-                      <Col xs="8">{this.state.profileInfo.brand}</Col>
+                      <Col xs="8">{profile.nickname}</Col>
                     </Row>
                   </div>
-                  <div className="text-left">
-                    Petite tenue pour cet hiver. Jupe et pull dispo. Les
-                    collants: du basique noir.
-                  </div>
+                  <div className="text-left">{clothing.description}</div>
                   <ul className="text-left list-unstyled list-inline">
                     <li className="list-inline-item">#jupe</li>
                     <li className="list-inline-item">#casual</li>
                     <li className="list-inline-item">#winteriscoming</li>
                   </ul>
                 </div>
-                {this.state.profileInfo.isCaution ? (
+                {clothing.is_deposit ? (
                   <section className="my-3 text-left">Caution demandée</section>
                 ) : null}
-                {/* <h2>{this.state.commentsNumber} Commentaires</h2> */}
+                <h2>{clothing.commentsNumber} Commentaires</h2>
                 <div className="comments-feed">
-                  {/* <Comment info={this.state.profileInfo} />
-                  <Comment info={this.state.profileInfo} /> */}
+                  <Comment info={profile} />
                 </div>
               </div>
               <Form className="comment-form">
