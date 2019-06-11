0dd6cba91b6539f8bff9e394b2026c657d262ac8
Sabine Robart <sabine.robart@protonmail.com>
2019-06-11 17:32:14 +0200

    [Refa] import infos from database into the Comment component.

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -2,24 +2,49 @@ import React from "react";
 import "../style/Comment.scss";
 import ReportButton from "./ReportButton";
 import { Row, Col } from "reactstrap";
+import axios from "axios";
 
 class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentInfo: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5050/comments`).then(({ data }) => {
+      this.setState({
+        commentInfo: data
+      });
+    });
+  }
+
   render() {
-    const info = this.props.info;
+    const comments = this.state.commentInfo;
+    const profile = this.props.info;
     return (
       <React.Fragment>
-        <Row className="comment-line mx-1 my-2">
-          <Col xs="2">
-            <img src={info.avatar} alt={`user-${info.id}`} className="avatar" />
-          </Col>
-          <Col xs="2">{info.username}</Col>
-          <Col xs="7" className="text-left">
-            {info.comment}
-          </Col>
-          <Col xs="1" className="p-0">
-            <ReportButton />
-          </Col>
-        </Row>
+        {comments.map((comment, key) => {
+          return (
+            <Row className="px-3 py-1">
+              <Col xs="2">
+                <img
+                  src={profile.avatar}
+                  alt={`user-${profile.id}`}
+                  className="avatar"
+                />
+              </Col>
+              <Col xs="2">{profile.nickname}</Col>
+              <Col xs="6" key={key}>
+                {comment.content}
+              </Col>
+              <Col xs="1">
+                <ReportButton />
+              </Col>
+            </Row>
+          );
+        })}
       </React.Fragment>
     );
   }
