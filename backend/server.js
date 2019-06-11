7628027c91d30018001a3976e22e44e85d2056fa
Sabine Robart <sabine.robart@protonmail.com>
2019-06-11 17:34:47 +0200

    [Add] basic express routes for the clothing page and comment component.

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { portNumber, db } = require("./conf");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const msg = "Welcome on Freep!";
+  console.log(msg);
+  res.status(200).send(msg);
+});
+
+app.get("/articles", (req, res) => {
+  db.query("SELECT description, is_deposit FROM clothing", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("error when getting articles route");
+    }
+    res.status(200).send(rows);
+  });
+});
+
+app.get("/comments", (req, res) => {
+  db.query("SELECT content FROM comment", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("error when getting comments route");
+    }
+    res.status(200).send(rows);
+  });
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM user", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("error when getting users route");
+    }
+    res.status(200).send(rows);
+  });
+});
+
+app.listen(portNumber, () => {
+  console.log(`API root available at: http://localhost:${portNumber}/`);
+});
