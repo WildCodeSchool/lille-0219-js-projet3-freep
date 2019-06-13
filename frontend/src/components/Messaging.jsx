00f4875a16aae86b4019902fe5394ef935d77bc3
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 15:34:45 +0200

    install a Row for the name and the date

--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardBody, CardText } from "reactstrap";
+import { Row } from "reactstrap";
 import "../pictures/Avatar.jpg";
 
 class Messaging extends React.Component {
@@ -9,14 +10,16 @@ class Messaging extends React.Component {
         <Card>
           <CardBody>
             <img src={Avatar} alt="Avatar" />
-            <p>FirstName LASTNAME</p>
+            <Row>
+              <p>FirstName LASTNAME</p>
+              <p>Il y a 5 minutes.</p>
+            </Row>
             <p>
               LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur adipiscing
               elit. Suspendisse aliquam facilisis mollis. Ut blandit eget turpis
               eu placerat. Nam ultrices purus quam, vitae accumsan leo commodo
               id. Proin non leo quis dui tincidunt porttitor.
             </p>
-            <p>Il y a 5 minutes.</p>
           </CardBody>
         </Card>
       </React.Fragment>
