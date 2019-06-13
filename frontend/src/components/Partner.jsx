9fe7257e22a637362ad6d796b2ad214339690b09
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 11:36:33 +0200

    creating a css for the Partner and partnerPage components.

--- frontend/src/components/Partner.jsx
+++ frontend/src/components/Partner.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import BannerKiabi from "../pictures/BannerKiabi.jpg";
+import "../style/Partner.css";
 
 class Partner extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <img src={BannerKiabi} alt="Kiabi" />
-        <p>Kiabi</p>
+        <div className="partner">
+          <img src={BannerKiabi} alt="Kiabi" className="banner" />
+          <p className="brand">Kiabi</p>
+        </div>
       </React.Fragment>
     );
   }
--- frontend/src/components/PartnerPage.jsx
+++ frontend/src/components/PartnerPage.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Partner from "./Partner";
-import { Card, CardText, CardBody } from "reactstrap";
+import { Card, CardText, CardBody, Container } from "reactstrap";
+import "../style/PartnerPage.css";
 class PartnerPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Card>
+        <Card className="cardPoint">
           <CardBody>
             <CardText>Vous avez N points.</CardText>
           </CardBody>
         </Card>
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
+        <Container>
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+          <Partner />
+        </Container>
       </React.Fragment>
     );
   }
