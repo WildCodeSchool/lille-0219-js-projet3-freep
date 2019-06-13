18568b2559b5d01c8f51bdb33296077760a48822
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 14:48:00 +0200

    create a MessagingPage component.

---
+++ frontend/src/components/MessagingPage.jsx
@@ -0,0 +1,23 @@
+import react from "react";
+import Messaging from "./Messaging";
+
+class MessagingPage extends react.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+        <Messaging />
+      </React.Fragment>
+    );
+  }
+}
+
+export default MessagingPage;
