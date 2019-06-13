6ba4b0751314fd1ec1847d1cc8d2d10a22dea443
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 10:58:35 +0200

    Fill the component with fake content

--- frontend/src/components/Partner.jsx
+++ frontend/src/components/Partner.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import BannerKiabi from "../pictures/BannerKiabi.jpg";
 
 class Partner extends React.Component {
-    render(){
-        return (
-
-        )
-    }
-};
+  render() {
+    return (
+      <React.Fragment>
+        <img src={BannerKiabi} alt="Kiabi" />
+        <p>Kiabi</p>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Partner;
