3205bc6049d885a92b393eb22bf009e487daa38a
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 15:03:58 +0200

    Fill the Messaging component.

--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Card, CardBody, CardText } from "reactstrap";
+import "../pictures/Avatar.jpg";
 
 class Messaging extends React.Component {
-    render(){
-        return (
-            
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Card>
+          <CardBody>
+            <img src={Avatar} alt="Avatar" />
+            <p>FirstName LASTNAME</p>
+            <p>
+              LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Suspendisse aliquam facilisis mollis. Ut blandit eget turpis
+              eu placerat. Nam ultrices purus quam, vitae accumsan leo commodo
+              id. Proin non leo quis dui tincidunt porttitor.
+            </p>
+            <p>Il y a 5 minutes.</p>
+          </CardBody>
+        </Card>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Messaging;
