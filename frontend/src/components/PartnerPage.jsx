ae00f157547c2cc5c641546fb7f1ab3865570ce2
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 10:42:04 +0200

    create a partnerPage component

---
+++ frontend/src/components/PartnerPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Partner from "./Partner";
+class PartnerPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Partner />
+      </React.Fragment>
+    );
+  }
+}
+
+export default PartnerPage;
