c5a036e646577f09a936f4d335739ccfd9b0c7b4
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-13 11:03:33 +0200

    Creating a card for the points.

--- frontend/src/components/PartnerPage.jsx
+++ frontend/src/components/PartnerPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Partner from "./Partner";
+import { Card, CardText, CardBody } from "reactstrap";
 class PartnerPage extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <Card>
+          <CardBody>
+            <CardText>Vous avez N points.</CardText>
+          </CardBody>
+        </Card>
         <Partner />
         <Partner />
         <Partner />
