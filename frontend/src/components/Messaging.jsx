fceacd1fbc2a411ce3dff9cebc7371c64ab5c5fa
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-14 11:56:03 +0200

    link component Message to component Messaging with router.

--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardBody, CardText, CardImg } from "reactstrap";
 import { Row } from "reactstrap";
 import "../style/Messaging.css";
+import { Link } from "react-router-dom";
 
 class Messaging extends React.Component {
   constructor(props) {
@@ -11,32 +12,34 @@ class Messaging extends React.Component {
     return (
       <React.Fragment>
         <Card>
-          <CardBody>
-            <CardImg
-              src={this.props.avatar}
-              alt="Avatar"
-              className="imgAvatar"
-            />
-            <Row>
-              <CardText className="cardTextMessaging">
-                <Row>
-                  <p>
-                    {this.props.firstname} {this.props.lastname}
-                  </p>
-                  <p className="messageDate">Il y a 5 minutes.</p>
-                </Row>
-                <Row>
-                  <p>
-                    LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                    blandit eget turpis eu placerat. Nam ultrices purus quam,
-                    vitae accumsan leo commodo id. Proin non leo quis dui
-                    tincidunt porttitor.
-                  </p>
-                </Row>
-              </CardText>
-            </Row>
-          </CardBody>
+          <Link to="/message">
+            <CardBody>
+              <CardImg
+                src={this.props.avatar}
+                alt="Avatar"
+                className="imgAvatar"
+              />
+              <Row>
+                <CardText className="cardTextMessaging">
+                  <Row>
+                    <p>
+                      {this.props.firstname} {this.props.lastname}
+                    </p>
+                    <p className="messageDate">Il y a 5 minutes.</p>
+                  </Row>
+                  <Row>
+                    <p>
+                      LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
+                      blandit eget turpis eu placerat. Nam ultrices purus quam,
+                      vitae accumsan leo commodo id. Proin non leo quis dui
+                      tincidunt porttitor.
+                    </p>
+                  </Row>
+                </CardText>
+              </Row>
+            </CardBody>
+          </Link>
         </Card>
       </React.Fragment>
     );
