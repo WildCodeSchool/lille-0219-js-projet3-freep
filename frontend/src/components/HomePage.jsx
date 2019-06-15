3397d99f70d1fb93768a946e1b73dbfe3033de73
Sabine Robart <sabine.robart@protonmail.com>
2019-06-15 09:15:00 +0200

    [Add] import pictures from BDD + map the results.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,41 +1,34 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 import Photo from "./Photo";
+import axios from "axios";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      picturesInfo: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5050/articles/`).then(({ data }) => {
+      this.setState({
+        picturesInfo: data
+      });
+    });
+  }
   render() {
+    const pictures = this.state.picturesInfo;
     return (
       <Row>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
-        <Col sm="6" md="4" lg="3">
-          <Photo />
-        </Col>
+        {pictures.map((picture, key) => {
+          return (
+            <Col sm="6" md="4" lg="3" key={key}>
+              <Photo picture={picture.url} />
+            </Col>
+          );
+        })}
       </Row>
     );
   }
