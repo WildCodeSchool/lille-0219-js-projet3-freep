c00459798e8d846801e11755074bf972eedfa145
Nathan Marcy <nathan.marcy@live.fr>
2019-06-16 15:37:42 +0200

    Trying to resolve conflic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,7 @@ function App() {
           <Route path="/profil/:profilId" component={Profile} />
           <Route path="/partenaire" component={PartnerPage} />
           <Route path="/messagerie" component={MessagingPage} />
+          <Route path="/message" component={Message} />
         </Switch>
       </Container>
     </React.Fragment>
