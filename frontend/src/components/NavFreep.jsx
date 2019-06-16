993b779b344228f2124547e2b8dcbbbbc33c8942
Nathan Marcy <nathan.marcy@live.fr>
2019-06-16 15:10:39 +0200

    Finalize integration of the navBar.

--- frontend/src/components/NavFreep.jsx
+++ frontend/src/components/NavFreep.jsx
@@ -26,23 +26,21 @@ class NavFreep extends React.Component {
               />
             </Link>
           </Navbar>
-          <div className="title">
-            <span>Freep</span>
+          <div className="navText">
+            <span className="navTitle">Freep</span>
             <br />
-            <span>La garde robe qui rapporte</span>
+            <span className="navCatch">La garde robe qui rapporte</span>
           </div>
           <NavbarToggler onClick={this.toggle} />
-          <Form className="search-input">
-            <InputGroup>
-              <InputGroupAddon addonType="prepend" />
-              <button id="loupe" type="submit" class="icone-loupe" />
-              <Input
-                className="search"
-                placeholder="Rechercher un vêtement ou une cliente"
-              />
-            </InputGroup>
-          </Form>
           <Collapse navbar>
+            <form action="">
+              <label for="">
+                <input type="text" placeholder="Ma super recherche" />
+                <button type="submit">
+                  <img src="../pictures/loupe.png" alt="magnifier" />
+                </button>
+              </label>
+            </form>
             <Nav className="ml-auto" navbar>
               <NavLink to="/">
                 <Plus className="img" color="black" />
