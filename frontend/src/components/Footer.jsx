2976652625a3d1fff4156ac3e973b077b4c2672f
Maryline Alves <malves.16@laposte.net>
2019-06-17 09:16:27 +0200

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,23 +5,20 @@ import '../style/Footer.css';
 class Footer extends React.Component {
 	render() {
 		return (
-			<Fragment>
+			<footer>
 				<NavLink activeClassName="active" className="littleInfo" exact to="/madewith">
 					made with :cœur:
 				</NavLink>
-				<br />
 				<NavLink activeClassName="active" className="littleInfo" exact to="/CGU">
 					CGU
 				</NavLink>
-				<br />
 				<NavLink activeClassName="active" className="littleInfo" exact to="/nouscontacter">
 					nous contacter
 				</NavLink>
-				<br />
 				<NavLink activeClassName="active" className="littleInfo" exact to="/quisommesnous">
 					qui sommes-nous ?
 				</NavLink>
-			</Fragment>
+			</footer>
 		);
 	}
 }
