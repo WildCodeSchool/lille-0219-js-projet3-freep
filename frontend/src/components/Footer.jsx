a6061d785fbbd0dcc5c8cbe109580f2c2adda333
Maryline Alves <malves.16@laposte.net>
2019-06-17 16:40:55 +0200

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,24 +1,41 @@
-import React, { Fragment } from 'react';
-import { NavLink } from 'react-router-dom';
-import '../style/Footer.css';
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import "../style/Footer.css";
 
 class Footer extends React.Component {
-	render() {
-		return (
-			<footer className="fixed-bottom">
-				<NavLink className="littleInfo">Made with love by Wild Code School</NavLink>
-				<NavLink activeClassName="active" className="littleInfo" exact to="/CGU">
-					CGU
-				</NavLink>
-				<NavLink activeClassName="active" className="littleInfo" exact to="/nouscontacter">
-					Nous contacter
-				</NavLink>
-				<NavLink activeClassName="active" className="littleInfo" exact to="/quisommesnous">
-					Qui sommes-nous ?
-				</NavLink>
-			</footer>
-		);
-	}
+  render() {
+    return (
+      <footer className="fixed-bottom">
+        <NavLink className="littleInfo">
+          Made with love by Wild Code School
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          className="littleInfo"
+          exact
+          to="/CGU"
+        >
+          CGU
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          className="littleInfo"
+          exact
+          to="/nouscontacter"
+        >
+          Nous contacter
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          className="littleInfo"
+          exact
+          to="/quisommesnous"
+        >
+          Qui sommes-nous ?
+        </NavLink>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
