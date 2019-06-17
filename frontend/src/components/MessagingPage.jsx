dccb32c4a60085f44774ae7bd94a91e6388417a3
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-17 17:39:11 +0200

    Fixing message if array is null.

--- frontend/src/components/MessagingPage.jsx
+++ frontend/src/components/MessagingPage.jsx
@@ -33,19 +33,37 @@ class MessagingPage extends React.Component {
     return (
       <React.Fragment>
         <h1>Messagerie</h1>
+        {this.state.messagingArray.length === 0 ? (
+          <p>Vous n'avez pas de conversation.</p>
+        ) : (
+          ""
+        )}
         {this.state.messagingArray.map((messaging, i) => {
-          console.log("messaging:" + messaging.nickname);
-          return (
-            messaging && (
-              <Messaging
-                key={i}
-                nickname={messaging.nickname}
-                avatar={messaging.avatar}
-                timeStamp={messaging.created_at}
-                message={messaging.content}
-              />
-            )
-          );
+          console.log("id: " + messaging.id);
+          console.log("idAuthor: " + messaging.id_author);
+          console.log("idReader: " + messaging.id_reader);
+          console.log("content: " + messaging.content);
+
+          if (messaging.content === undefined) {
+            console.log("Vous n'avez pas de conversation.");
+            return <p>Vous n'avez pas de conversation.</p>;
+          } else {
+            return (
+              messaging && (
+                <Messaging
+                  key={i}
+                  nickname={messaging.nickname}
+                  avatar={messaging.avatar}
+                  timeStamp={
+                    messaging.date_diff >= 1
+                      ? "Il y a " + messaging.date_diff + " jours."
+                      : "Envoyé à " + messaging.hour_send + "."
+                  }
+                  message={messaging.content}
+                />
+              )
+            );
+          }
         })}
       </React.Fragment>
     );
