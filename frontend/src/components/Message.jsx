7ba33f7bd7379a854fcf69be51e5a46b4a8b8f6b
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-06-17 09:30:17 +0200

    finish CSS for message page plus import the montserrat font-family

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Card, CardBody, CardText, CardImg } from "reactstrap";
-import { Row } from "reactstrap";
+import { Card, CardBody, CardText, CardImg, Row, Col } from "reactstrap";
 import "../style/Messaging.scss";
 import Ruby from "../pictures/Ruby.jpg";
 import Scarlett from "../pictures/Scarlett.jpg";
@@ -11,83 +10,139 @@ class Message extends React.Component {
       <React.Fragment>
         <Card>
           <CardBody>
-            <CardImg src={Ruby} alt="Avatar" className="imgAvatar" />
             <Row>
-              <CardText className="cardTextMessaging">
+              <Col xs="3" sm="3" md="3">
+                <CardImg
+                  src={Ruby}
+                  alt="Avatar"
+                  className="imgAvatar rounded-circle"
+                />
+              </Col>
+              <Col xs="9" sm="9" md="9">
                 <Row>
-                  <p className="name">Ruby Rose</p>
-                  <p className="messageDate">Il y a 5 minutes.</p>
+                  <CardText className="m-5">
+                    <Row className="d-flex">
+                      <Col>
+                        <p className="name">Ruby Rose</p>
+                      </Col>
+                      <Col>
+                        <p className="messageDate">Il y a 5 minutes.</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <p className="bodyText text-justify">
+                        LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse aliquam facilisis mollis.
+                        Ut blandit eget turpis eu placerat. Nam ultrices purus
+                        quam, vitae accumsan leo commodo id. Proin non leo quis
+                        dui tincidunt porttitor.
+                      </p>
+                    </Row>
+                  </CardText>
                 </Row>
-                <Row>
-                  <p>
-                    LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                    blandit eget turpis eu placerat. Nam ultrices purus quam,
-                    vitae accumsan leo commodo id. Proin non leo quis dui
-                    tincidunt porttitor.
-                  </p>
-                </Row>
-              </CardText>
+              </Col>
             </Row>
           </CardBody>
           <CardBody>
-            <CardImg src={Scarlett} alt="Avatar" className="imgAvatarRight" />
             <Row>
-              <CardText className="cardTextMessaging">
+              <Col xs="9" sm="9" md="9">
                 <Row>
-                  <p>Il y a 5 minutes.</p>
-                  <p className="nameRight name">Moi</p>
+                  <CardText className="m-5">
+                    <Row className="d-flex">
+                      <Col>
+                        <p>Il y a 5 minutes.</p>
+                      </Col>
+                      <Col>
+                        <p className="name nameRight">Moi</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <p className="bodyText text-justify">
+                        LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse aliquam facilisis mollis.
+                        Ut blandit eget turpis eu placerat. Nam ultrices purus
+                        quam, vitae accumsan leo commodo id. Proin non leo quis
+                        dui tincidunt porttitor.
+                      </p>
+                    </Row>
+                  </CardText>
                 </Row>
-                <Row>
-                  <p>
-                    LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                    blandit eget turpis eu placerat. Nam ultrices purus quam,
-                    vitae accumsan leo commodo id. Proin non leo quis dui
-                    tincidunt porttitor.
-                  </p>
-                </Row>
-              </CardText>
+              </Col>
+              <Col xs="3" sm="3" md="3">
+                <CardImg
+                  src={Scarlett}
+                  alt="Avatar"
+                  className="imgAvatar rounded-circle"
+                />
+              </Col>
             </Row>
           </CardBody>
           <CardBody>
-            <CardImg src={Ruby} alt="Avatar" className="imgAvatar" />
             <Row>
-              <CardText className="cardTextMessaging">
+              <Col xs="3" sm="3" md="3">
+                <CardImg
+                  src={Ruby}
+                  alt="Avatar"
+                  className="imgAvatar rounded-circle"
+                />
+              </Col>
+              <Col xs="9" sm="9" md="9">
                 <Row>
-                  <p className="name">Ruby Rose</p>
-                  <p className="messageDate">Il y a 5 minutes.</p>
+                  <CardText className="m-5">
+                    <Row className="d-flex">
+                      <Col>
+                        <p className="name">Ruby Rose</p>
+                      </Col>
+                      <Col>
+                        <p className="messageDate">Il y a 5 minutes.</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <p className="bodyText text-justify">
+                        LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse aliquam facilisis mollis.
+                        Ut blandit eget turpis eu placerat. Nam ultrices purus
+                        quam, vitae accumsan leo commodo id. Proin non leo quis
+                        dui tincidunt porttitor.
+                      </p>
+                    </Row>
+                  </CardText>
                 </Row>
-                <Row>
-                  <p>
-                    LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                    blandit eget turpis eu placerat. Nam ultrices purus quam,
-                    vitae accumsan leo commodo id. Proin non leo quis dui
-                    tincidunt porttitor.
-                  </p>
-                </Row>
-              </CardText>
+              </Col>
             </Row>
           </CardBody>
           <CardBody>
-            <CardImg src={Scarlett} alt="Avatar" className="imgAvatarRight" />
             <Row>
-              <CardText className="cardTextMessaging">
+              <Col xs="9" sm="9" md="9">
                 <Row>
-                  <p>Il y a 5 minutes.</p>
-                  <p className="nameRight name">Moi</p>
+                  <CardText className="m-5">
+                    <Row className="d-flex">
+                      <Col>
+                        <p>Il y a 5 minutes.</p>
+                      </Col>
+                      <Col>
+                        <p className="name nameRight">Moi</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <p className="bodyText text-justify">
+                        LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Suspendisse aliquam facilisis mollis.
+                        Ut blandit eget turpis eu placerat. Nam ultrices purus
+                        quam, vitae accumsan leo commodo id. Proin non leo quis
+                        dui tincidunt porttitor.
+                      </p>
+                    </Row>
+                  </CardText>
                 </Row>
-                <Row>
-                  <p>
-                    LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                    blandit eget turpis eu placerat. Nam ultrices purus quam,
-                    vitae accumsan leo commodo id. Proin non leo quis dui
-                    tincidunt porttitor.
-                  </p>
-                </Row>
-              </CardText>
+              </Col>
+              <Col xs="3" sm="3" md="3">
+                <CardImg
+                  src={Scarlett}
+                  alt="Avatar"
+                  className="imgAvatar rounded-circle"
+                />
+              </Col>
             </Row>
           </CardBody>
         </Card>
--- frontend/src/components/Messaging.jsx
+++ frontend/src/components/Messaging.jsx
@@ -1,43 +1,53 @@
 import React from "react";
-import { Card, CardBody, CardText, CardImg } from "reactstrap";
-import { Row } from "reactstrap";
+import { Card, CardBody, CardText, CardImg, Row, Col } from "reactstrap";
 import "../style/Messaging.scss";
 import { Link } from "react-router-dom";
 
 class Messaging extends React.Component {
   constructor(props) {
     super(props);
+    this.state = [];
   }
 
   render() {
     return (
       <React.Fragment>
-        <Card info={this.props}>
-          <Link to="/message">
+        <Card>
+          <Link to="/message" className="link">
             <CardBody>
-              <CardImg
-                src={this.props.avatar}
-                alt="Avatar"
-                className="imgAvatar"
-              />
               <Row>
-                <CardText className="cardTextMessaging">
+                <Col xs="3" sm="3" md="3">
+                  <CardImg
+                    src={this.props.avatar}
+                    alt="Avatar"
+                    className="imgAvatar rounded-circle"
+                  />
+                </Col>
+                <Col xs="9" sm="9" md="9">
                   <Row>
-                    <p className="name">
-                      {this.props.firstname} {this.props.lastname}
-                    </p>
-                    <p className="messageDate">Il y a 5 minutes.</p>
+                    <CardText className="m-5">
+                      <Row className="d-flex">
+                        <Col>
+                          <p className="name">
+                            {this.props.firstname} {this.props.lastname}
+                          </p>
+                        </Col>
+                        <Col>
+                          <p className="messageDate">Il y a 5 minutes.</p>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <p className="bodyText text-justify">
+                          LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
+                          adipiscing elit. Suspendisse aliquam facilisis mollis.
+                          Ut blandit eget turpis eu placerat. Nam ultrices purus
+                          quam, vitae accumsan leo commodo id. Proin non leo
+                          quis dui tincidunt porttitor.
+                        </p>
+                      </Row>
+                    </CardText>
                   </Row>
-                  <Row>
-                    <p>
-                      LAST MESSAGE. Lorem ipsum dolor sit amet, consectetur
-                      adipiscing elit. Suspendisse aliquam facilisis mollis. Ut
-                      blandit eget turpis eu placerat. Nam ultrices purus quam,
-                      vitae accumsan leo commodo id. Proin non leo quis dui
-                      tincidunt porttitor.
-                    </p>
-                  </Row>
-                </CardText>
+                </Col>
               </Row>
             </CardBody>
           </Link>
