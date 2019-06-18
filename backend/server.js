231a1a090596fd19207c47135575082561d037fc
Sabine Robart <sabine.robart@protonmail.com>
2019-06-18 11:10:39 +0200

    [Fix] Sort pictures by date (newest to oldest) on the homepage.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.get("/articles/", (req, res) => {
   db.query(
-    `SELECT id, id_clothing, id_user, is_proof, created_at, url FROM picture`,
+    `SELECT id, id_clothing, id_user, is_proof, created_at, url FROM picture ORDER BY created_at DESC`,
     (err, rows) => {
       if (err) {
         console.log(err);
