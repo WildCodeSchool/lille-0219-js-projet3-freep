db602c065d9cf42bb37a14be2b6a54447bf3805b
Sabine Robart <sabine.robart@protonmail.com>
2019-06-18 17:10:54 +0200

    [Del] code not needed for now.

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -3,33 +3,8 @@ import { Heart, Target } from "react-feather";
 import { Link } from "react-router-dom";
 import { Row, Card, CardImg } from "reactstrap";
 import ReportButton from "./ReportButton";
-import axios from "axios";
 
 class Photo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLiked: "0"
-    };
-  }
-
-  componentDidMount() {
-    //   axios
-    //     .delete(`http://localhost:5050/social/${pictureId}`)
-    //     .then(({ data }) => {
-    //       this.setState({
-    //         isLiked: data
-    //       });
-    //     });
-    // }
-    // like(e) {
-    //   axios.post(`http://localhost:5050/social/${pictureId}`).then(({ data }) => {
-    //     this.setState({
-    //       isLiked: data
-    //     });
-    //   });
-  }
-
   render() {
     const picture = this.props.picture;
     const link = this.props.link;
@@ -38,14 +13,11 @@ class Photo extends React.Component {
       <React.Fragment>
         <Card>
           <Link to={`/article/${link}`}>
-            <CardImg src={picture.url} alt="clothes" className="Photo" />
+            <CardImg src={picture} alt="clothes" className="Photo" />
           </Link>
           <div className="overlay">
             <Row className="p-0 card-buttons align-items-center">
-              <Heart
-                color="white"
-                onClick={e => this.like(this.props.pictureId)}
-              />
+              <Heart color="white" />
               <Target color="white" />
               <ReportButton />
             </Row>
