7e3ed7e665eaaf404f0d97aae73588f1b833867b
Sabine Robart <sabine.robart@protonmail.com>
2019-06-19 12:16:13 +0200

    [Fix] avatar/name responsiveness on the clothing page.

--- frontend/src/components/ClothingPage.jsx
+++ frontend/src/components/ClothingPage.jsx
@@ -225,7 +225,7 @@ class ClothingPage extends React.Component {
                 <div className="p-4 comments-feed">
                   <div>
                     <Row className="align-items-center">
-                      <Col xs="3">
+                      <Col xs="6" md="5">
                         <img
                           src={profile.avatar}
                           alt={`user-${profile.id}`}
@@ -233,7 +233,7 @@ class ClothingPage extends React.Component {
                           width="70px"
                         />
                       </Col>
-                      <Col xs="9" className="profile-name">
+                      <Col xs="6" md="7" className="profile-name">
                         {profile.nickname}
                       </Col>
                     </Row>
