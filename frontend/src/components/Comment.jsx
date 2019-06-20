9946d505a2f0e2c9ade9d0cb291ea5b213a0d915
Sabine Robart <sabine.robart@protonmail.com>
2019-06-20 21:48:13 +0200

    [Refa] the Comment component to turn it into a stateless component (performance reasons).

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -3,34 +3,31 @@ import "../style/Comment.scss";
 import ReportButton from "./ReportButton";
 import { Row, Col } from "reactstrap";
 
-class Comment extends React.Component {
-  render() {
-    const comment = this.props.comment;
-    const profile = this.props.profile;
-    return (
-      <Row className="px-3 py-1 align-items-center">
-        <Col xs="2">
-          <img
-            src={profile.avatar}
-            alt={`user-${profile.id}`}
-            className="avatar"
-          />
-        </Col>
-        <Col
-          xs="2"
-          className="align-content-center profile-name comments-profile"
-        >
-          {profile.nickname}
-        </Col>
-        <Col xs="6" className="align-items-center">
-          {comment.content}
-        </Col>
-        <Col xs="1" className="align-items-center">
-          <ReportButton />
-        </Col>
-      </Row>
-    );
-  }
-}
+const Comment = props => {
+  return (
+    <Row className="px-3 py-1 align-items-center">
+      <Col xs="2">
+        <img
+          src={props.profile.avatar}
+          alt={`user-${props.profile.id}`}
+          className="avatar"
+          width="20"
+        />
+      </Col>
+      <Col
+        xs="2"
+        className="align-content-center profile-name comments-profile"
+      >
+        {props.profile.nickname}
+      </Col>
+      <Col xs="6" className="align-items-center">
+        {props.comment.content}
+      </Col>
+      <Col xs="1" className="align-items-center">
+        <ReportButton />
+      </Col>
+    </Row>
+  );
+};
 
 export default Comment;
