59f6b9c83ad6963f3bfdce6f583aa74f1464476c
Nathan Marcy <nathan.marcy@live.fr>
2019-06-24 17:44:29 +0200

    Fixing somes issues.

--- frontend/src/components/NavFreep.jsx
+++ frontend/src/components/NavFreep.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Navbar, Nav, NavbarToggler, Collapse } from "reactstrap";
 import { Link, NavLink } from "react-router-dom";
-import { Upload, Mail, Heart, User } from "react-feather";
+import { Tag, Upload, Mail, Heart, User } from "react-feather";
 import { Modal } from "reactstrap";
 import Uploader from "./Upload";
 
 class NavFreep extends React.Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
+    this.toggleBurger = this.toggleBurger.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
     this.state = {
       isOpen: false,
       modal: false
     };
   }
-  toggle() {
+  toggleBurger() {
     this.setState({
       isOpen: !this.state.isOpen
     });
+  }
+  toggleModal() {
     this.setState(prevState => ({
       modal: !prevState.modal
     }));
@@ -27,17 +30,15 @@ class NavFreep extends React.Component {
     return (
       <div className="header">
         <Navbar color="light" light expand="md">
-          <Navbar color="black" light>
-            <Link to="/">
-              <img className="logo" src="../pictures/logo.png" alt="logo" />
-            </Link>
-          </Navbar>
+          <Link to="/">
+            <img className="logo" src="../pictures/logo.png" alt="logo" />
+          </Link>
           <div className="navText">
             <span className="navTitle">Freep</span>
             <br />
             <span className="navCatch">La garde robe qui rapporte</span>
           </div>
-          <NavbarToggler onClick={this.toggle} />
+          <NavbarToggler onClick={this.toggleBurger} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <form action="">
               <label htmlFor="clothe-profile-search">
@@ -50,7 +51,11 @@ class NavFreep extends React.Component {
             </form>
             <Nav className="ml-auto" navbar>
               <NavLink title="Propose ton vêtement !">
-                <Upload className="img" color="black" onClick={this.toggle} />
+                <Upload
+                  className="img"
+                  color="black"
+                  onClick={this.toggleModal}
+                />
                 <Modal isOpen={this.state.modal} toggle={this.toggle}>
                   <Uploader />
                 </Modal>
@@ -58,7 +63,7 @@ class NavFreep extends React.Component {
               <NavLink to="/partenaire">
                 <Tag className="img" color="black" />
               </NavLink>
-              <NavLink to="/messagerie/1">
+              <NavLink to="/messagerie/2">
                 <Mail className="img" color="black" />
               </NavLink>
               <NavLink to="/favoris">
