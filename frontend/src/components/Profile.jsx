edd439ceae52be8acafb45219f0b1cde93d83061
Richard Gaillet <gaillet.richard@gmail.com>
2019-06-24 12:04:18 +0200

    Adding Social elements (Followers, Following & Posts).

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 import Avatar from "./Avatar";
 import Nickname from "./Nickname";
 import Photo from "./Photo";
@@ -15,77 +16,111 @@ class Profile extends React.Component {
         avatar: "https://randomuser.me/api/portraits/women/90.jpg",
         nickname: "Jade",
         description: `Qu'est-ce que le Lorem Ipsum ?
-        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.`,
-        followers: 127,
-        following: 31,
-        posts: 10
+        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.`
       },
-      pictures: []
+      pictures: [],
+      social: [
+        {
+          nbFollowers: 23333
+        },
+        {
+          nbFollowing: 313445
+        },
+        {
+          nbPosts: 10923
+        }
+      ]
     };
   }
 
   componentDidMount() {
     const profileId = this.props.match.params.profileId;
 
-    axios.get(`http://localhost:5050/profile/${profileId}`).then(({ data }) => {
-      this.setState({
-        user: data.profile,
-        pictures: data.pictures
-      });
+    axios.get(`http://localhost:5050/profil/${profileId}`).then(({ data }) => {
+      this.setState(
+        {
+          user: data.profile,
+          pictures: data.pictures,
+          social: data.social
+        },
+        () => {
+          console.log(data.social);
+        }
+      );
     });
   }
 
   render() {
     const user = this.state.user;
     const pictures = this.state.pictures;
+    const social = this.state.social;
 
     return (
       <React.Fragment>
         <Container>
-          <Row className="text-center align-items-center">
-            <Col md="3" className="text-center mx-auto">
-              <Avatar info={user} />
-            </Col>
-            <Col>
-              <Row>
-                <Col md="12" lg="3" className="d-flex align-items-center">
-                  <Col md="auto">
-                    ✮✮✮<span className="starsgrey">✮✮</span>
-                  </Col>
-                  <Col md="auto" className="nick">
-                    <Nickname info={user} />
-                  </Col>
+          <Row>
+            <Col xs="12" sm="6">
+              <Row className="align-items-center">
+                <Col xs="4" md="3">
+                  <Avatar info={user} />
                 </Col>
-                <Col
-                  md="8 offset-1"
-                  className="text-justify amatic p-3 description"
-                >
-                  {user.description}
+                <Col>
+                  <Row xs="6" className="align-items-center">
+                    <Col xs="auto">
+                      <Nickname info={user} />
+                    </Col>
+                    <Col xs="auto">✮✮✮✮✮</Col>
+                  </Row>
                 </Col>
               </Row>
             </Col>
+            <Col xs="12" sm="6" className="align-items-center d-flex">
+              <Col className="text-justify">{user.description}</Col>
+            </Col>
           </Row>
           <Row className="text-center">
-            <Col sm="6" md="4" xl="2" className="dancingscript py-3">
-              {user.followers} Followers
+            <Col xs="4" xl="2" className="primaryfont p-0">
+              {social[0]
+                ? social[0].nbFollowers > 9999
+                  ? (social[0].nbFollowers / 1000).toPrecision(3) +
+                    "K Followers"
+                  : social[0].nbFollowers < 2
+                  ? social[0].nbFollowers + " Follower"
+                  : social[0].nbFollowers + " Followers"
+                : 0 + " Follower"}
             </Col>
-            <Col sm="6" md="4" xl="2" className="dancingscript py-3">
-              {user.following} Following
+            <Col xs="4" xl="2" className="primaryfont p-0">
+              {social[1]
+                ? social[1].nbFollowing > 9999
+                  ? (social[1].nbFollowing / 1000).toPrecision(3) +
+                    "K Followers"
+                  : social[1].nbFollowing < 2
+                  ? social[1].nbFollowing + " Following"
+                  : social[1].nbFollowing + " Followings"
+                : 0 + " Following"}
             </Col>
-            <Col sm="12" md="4" xl="2" className="dancingscript py-3">
-              {user.posts} Posts
+            <Col xs="4" xl="2" className="primaryfont p-0">
+              {social[2]
+                ? social[2].nbPosts > 9999
+                  ? (social[2].nbPosts / 1000).toPrecision(3) + "K Posts"
+                  : social[2].nbPosts < 2
+                  ? social[2].nbPosts + " Post"
+                  : social[2].nbPosts + " Posts"
+                : 0 + " Post"}
             </Col>
-            <Col sm="6" xl="3">
-              <Button className="button m-3 amatic">Éditer</Button>
+            <Col>
+              <Button className="button primaryfont">Suivre</Button>
             </Col>
-            <Col sm="6" xl="3">
-              <Button className="button m-3 amatic">Message</Button>
+            <Col>
+              <Link to="/message">
+                <Button className="button primaryfont">Message</Button>
+              </Link>
             </Col>
           </Row>
           <Row>
             {pictures.map((picture, idx) => {
               return (
-                <Col sm="6" md="4" lg="3" xl="2" key={idx}>
+                <Col md="6" lg="3" key={idx}>
                   <Photo picture={picture.url} link={picture.id_clothing} />
                 </Col>
               );
