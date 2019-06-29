bbbb6f6f62debb4e9ea81f59c0cc4a7b42132408
Sabine Robart <sabine.robart@protonmail.com>
2019-06-29 09:01:40 +0200

    [Add] protected routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import Profile from "./components/Profile";
 import { Container } from "reactstrap";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ClothingPage from "./components/ClothingPage";
 import NavFreep from "./components/NavFreep";
 import "./style/NavFreep.scss";
@@ -16,6 +16,19 @@ import Registration from "./components/Registration";
 import Loader from "./components/Loader";
 import BorrowPage from "./components/BorrowPage";
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const token = localStorage.getItem("user");
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        token ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
 function App() {
   return (
     <React.Fragment>
@@ -25,17 +38,20 @@ function App() {
       <Container className="container">
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route path="/accueil" component={HomePage} />
-          <Route path="/article/:articleId" component={ClothingPage} />
-          <Route path="/profil/:profileId" component={Profile} />
+          <PrivateRoute path="/accueil" component={HomePage} />
+          <PrivateRoute path="/article/:articleId" component={ClothingPage} />
+          <PrivateRoute path="/profil/:profileId" component={Profile} />
           <Route path="/users/:userId" component={ClothingPage} />
-          <Route path="/partenaire" component={PartnerPage} />
-          <Route path="/messagerie/:idReader" component={MessagingPage} />
+          <PrivateRoute path="/partenaire" component={PartnerPage} />
+          <PrivateRoute
+            path="/messagerie/:idReader"
+            component={MessagingPage}
+          />
           <Route path="/login" exact component={Login} />
           <Route path="/registration" exact component={Registration} />
-          <Route path="/message/:P1/:P2" component={Message} />
+          <PrivateRoute path="/message/:P1/:P2" component={Message} />
           <Route path="/loader" component={Loader} />
-          <Route path="/emprunt/:userId" component={BorrowPage} />
+          <PrivateRoute path="/emprunt/:userId" component={BorrowPage} />
         </Switch>
         <Footer />
       </Container>
