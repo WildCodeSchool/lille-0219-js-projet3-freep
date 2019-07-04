9327bdfc07a08c18a3865693096f94e0ab71d688
Richard Gaillet <gaillet.richard@gmail.com>
2019-07-04 11:56:56 +0200

    Move useless code to Photo component.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -22,36 +22,22 @@ class HomePage extends React.Component {
         loading: false
       });
     });
-
-    const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
-    axios.get(`http://localhost:5050/like/${currentUser}`).then(({ data }) => {
-      this.setState({
-        picturesLiked: data
-      });
-    });
   }
 
   render() {
     const pictures = this.state.picturesInfo;
-    localStorage.getItem("user");
-    const picturesLiked = this.state.picturesLiked;
     if (this.state.loading) {
       return <Loader />;
     } else {
       return (
         <Masonry>
           {pictures.map((picture, key) => {
-            const liked =
-              picturesLiked && picturesLiked.indexOf(picture.id) !== -1
-                ? true
-                : false;
             return (
               <Col sm="6" md="4" lg="3" key={key}>
                 <Photo
                   picture={picture.url}
                   pictureId={picture.id}
                   link={picture.id_clothing}
-                  isLiked={liked}
                 />
               </Col>
             );
