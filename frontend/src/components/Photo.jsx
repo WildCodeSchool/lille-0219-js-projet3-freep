70f2e8b75b44695e03f7749ecd249edf735cd40d
Sabine Robart <sabine.robart@protonmail.com>
2019-07-06 11:01:34 +0200

    [Add] api call to get each picture show whether a deposit is needed or not

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -3,34 +3,43 @@ import { Heart, Target } from "react-feather";
 import { Link } from "react-router-dom";
 import { Row, Card, CardImg } from "reactstrap";
 import ReportButton from "./ReportButton";
+import axios from "axios";
 
 class Photo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deposit: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5050/deposit/`).then(({ data }) => {
+      this.setState({
+        deposit: data.deposit
+      });
+    });
+  }
+
   render() {
     const picture = this.props.picture;
     const link = this.props.link;
+    const deposit = this.state.deposit;
+    const dep = deposit && deposit.indexOf(link) !== -1 ? false : true;
+
     return (
-      <React.Fragment>
-        <Card className="m-2 picture-card">
-          <Link to={`/article/${link}`}>
-            <CardImg src={picture} alt="clothes" className="Photo" />
-          </Link>
-          <div className="overlay">
-            <Row className="p-0 card-buttons align-items-center">
-              <Heart color="white" />
-              <div
-                style={{
-                  color: "white",
-                  fontWeight: "bolder",
-                  fontSize: "1.5rem"
-                }}
-              >
-                ℂ
-              </div>
-              <ReportButton />
-            </Row>
-          </div>
-        </Card>
-      </React.Fragment>
+      <Card className="m-2 picture-card">
+        <Link to={`/article/${link}`}>
+          <CardImg src={picture} alt="clothes" className="Photo" />
+        </Link>
+        <div className="overlay">
+          <Row className="p-0 card-buttons align-items-center">
+            <Heart color="white" />
+            <div className={dep ? "deposit" : "no-deposit"}>ℂ</div>
+            <ReportButton />
+          </Row>
+        </div>
+      </Card>
     );
   }
 }
