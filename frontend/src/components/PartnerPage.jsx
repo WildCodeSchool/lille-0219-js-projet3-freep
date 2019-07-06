a85fb0b3334e2ae2f334ae8a60a653d2552a7bd7
Sabine Robart <sabine.robart@protonmail.com>
2019-07-06 12:50:00 +0200

    [Add] call api to get the user's points number

--- frontend/src/components/PartnerPage.jsx
+++ frontend/src/components/PartnerPage.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import Partner from "./Partner";
 import { Card, CardBody } from "reactstrap";
-import "../style/PartnerPage.css";
+import "../style/PartnerPage.scss";
+import axios from "axios";
+
 class PartnerPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userPoints: null
+    };
+  }
+
+  componentDidMount() {
+    const profileId = JSON.parse(localStorage.getItem("user")).user.id;
+    axios
+      .get(`http://localhost:5050/partenaire/${profileId}`)
+      .then(({ data }) => {
+        this.setState({
+          userPoints: data.points
+        });
+      });
+  }
+
   render() {
+    const points = this.state.userPoints;
     return (
       <React.Fragment>
         <Card className="cardPoint">
           <CardBody>
-            <p className="point">Vous avez N points.</p>
+            <p className="point">Vous avez {points} points.</p>
           </CardBody>
         </Card>
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
-        <Partner />
         <Partner />
       </React.Fragment>
     );
