19ad613dc9491f648bfd3b6fe5a45d80b03a601e
Sabine Robart <sabine.robart@protonmail.com>
2019-07-06 22:27:13 +0200

    [Refa] the Partner component into a functional component

--- frontend/src/components/Partner.jsx
+++ frontend/src/components/Partner.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import ComingSoon from "../pictures/comingsoon.png";
 import "../style/Partner.scss";
 
-class Partner extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="partner">
-          <img src={ComingSoon} alt="coming soon" className="banner" />
-          <p className="brand"> </p>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+const Partner = () => {
+  return (
+    <div className="partner">
+      <img src={ComingSoon} alt="coming soon" className="banner" />
+      <p className="brand"> </p>
+    </div>
+  );
+};
 
 export default Partner;
