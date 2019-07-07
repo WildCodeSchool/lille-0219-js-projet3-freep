5ee5f53aac885c77faedd46ab3f07b2611cb2cc4
Sabine Robart <sabine.robart@protonmail.com>
2019-07-07 18:46:26 +0200

    [Add] link to user profile in the comments section

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -3,22 +3,25 @@ import "../style/Comment.scss";
 import ReportButton from "./ReportButton";
 import { Row, Col } from "reactstrap";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const Comment = props => {
   const diff = moment(props.comment.created_at).format("LL");
   return (
     <Row className="px-3 py-1 my-2 align-items-center">
-      <Col md="2" xs="1">
-        <img
-          src={props.profile.avatar}
-          alt={`user-${props.profile.id}`}
-          className="comment-avatar"
-        />
-      </Col>
+      <Link to={`/profil/${props.profile.id}`}>
+        <Col md="2" xs="1">
+          <img
+            src={props.profile.avatar}
+            alt={`user-${props.profile.id}`}
+            className="comment-avatar"
+          />
+        </Col>
+      </Link>
       <Col
         md="2"
         xs="1"
-        className="align-content-center profile-name comments-profile"
+        className="align-content-center profile-name comments-profile m-0"
       >
         {props.profile.nickname}
       </Col>
