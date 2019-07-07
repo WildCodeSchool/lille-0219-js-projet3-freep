6d67137fbdb9347b1bbf52d0a90bb5bc251d1a7f
Sabine Robart <sabine.robart@protonmail.com>
2019-07-07 08:58:22 +0200

    [Del] duplicated code + [Add] links (WCS-contact)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import "../style/Footer.scss";
 import { Col } from "reactstrap";
 import classnames from "classnames";
 import { Heart } from "react-feather";
+import mailtoAddress from "../conf.js";
 
 class Footer extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       prevScrollpos: window.pageYOffset,
       visible: true
@@ -23,10 +23,8 @@ class Footer extends React.Component {
 
   handleScroll = () => {
     const { prevScrollpos } = this.state;
-
     const currentScrollPos = window.pageYOffset;
     const visible = prevScrollpos > currentScrollPos;
-
     this.setState({
       prevScrollpos: currentScrollPos,
       visible
@@ -36,7 +34,6 @@ class Footer extends React.Component {
   render() {
     return (
       <footer
-        className="row fixed-bottom p-1 justify-content-center m-0"
         className={classnames(
           "footer row fixed-bottom p-1 justify-content-center m-0",
           {
@@ -45,11 +42,13 @@ class Footer extends React.Component {
         )}
       >
         <Col xs="12" sm="3" className="text-center">
-          Made with
-          <span role="img" aria-label="" className="footer-heart" alt="love">
-            <Heart fill="#f6003f" color="transparent" />
-          </span>
-          by Wild Code School
+          <a href="https://wildcodeschool.fr/formation-developpeur-web-mobile/">
+            Made with
+            <span role="img" aria-label="" className="footer-heart" alt="love">
+              <Heart fill="#f6003f" color="transparent" />
+            </span>
+            by Wild Code School
+          </a>
         </Col>
         <Col
           xs="12"
@@ -60,7 +59,7 @@ class Footer extends React.Component {
             CGU
           </Col>
           <Col xs="4" sm="3" className="p-0">
-            Nous contacter
+            <a href={mailtoAddress}>Nous contacter</a>
           </Col>
           <Col xs="6" sm="3" className="p-0">
             Qui sommes-nous ?
