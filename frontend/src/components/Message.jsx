7bdd5411b3cb095df2ce6b30cb49047374dc89cb
Sabine Robart <sabine.robart@protonmail.com>
2019-07-08 10:35:42 +0200

    [Fix] general display + get the logged in user's infos in the messages page

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -20,25 +20,43 @@ class Message extends React.Component {
     super(props);
     this.state = {
       messageArray: [{}],
-      content_form: ""
+      content_form: "",
+      profile: null
     };
   }
 
   componentDidMount() {
-    const P1 = this.props.match.params.P1;
+    const user = JSON.parse(localStorage.getItem("user"));
     const P2 = this.props.match.params.P2;
-
-    axios.get(`http://localhost:5050/message/${P1}/${P2}`).then(({ data }) => {
-      this.setState({
-        messageArray: data
-      });
-    });
+    if ("user" in localStorage) {
+      const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
+      this.setState(
+        {
+          profile: currentUser
+        },
+        () => {
+          axios
+            .get(`http://localhost:5050/message/${this.state.profile}/${P2}`, {
+              headers: {
+                Authorization: `Bearer ${user.token}`
+              }
+            })
+            .then(({ data }) => {
+              this.setState({
+                messageArray: data
+              });
+            });
+        }
+      );
+    }
   }
 
   handlePrev(e) {
+    const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
     e.preventDefault();
-    this.props.history.goBack();
+    this.props.history.push(`/messagerie/${currentUser}`);
   }
+
   handleChange(e) {
     const { value } = e.target;
     this.setState({
@@ -48,28 +66,26 @@ class Message extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    const { content_form } = this.state;
     const P1 = this.props.match.params.P1;
     const P2 = this.props.match.params.P2;
-    const { content_form } = this.state;
+    const user = JSON.parse(localStorage.getItem("user"));
 
     axios
       .post(`http://localhost:5050/message/${P1}/${P2}`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        },
         content: content_form
       })
       .then(({ data }) => {
-        console.log(data);
         let messages = this.state.messageArray;
         data.hour_send = "l'instant";
         messages.unshift(data);
-        this.setState(
-          {
-            content_form: "",
-            messageArray: messages
-          },
-          () => {
-            console.log(this.state.messageArray);
-          }
-        );
+        this.setState({
+          content_form: "",
+          messageArray: messages
+        });
       })
       .catch(err => {
         console.log(`Nope! ${err}`);
@@ -77,41 +93,36 @@ class Message extends React.Component {
   }
 
   render() {
+    const currentUser = this.state.profile;
     return (
-      <div className="d-flex flex-column">
+      <Row className="messages-container p-0 m-0">
+        <Button
+          className="back-message-btn d-flex"
+          onClick={e => {
+            this.handlePrev(e);
+          }}
+        >
+          <ArrowLeft className="mx-2" /> Messagerie
+        </Button>
         <Form
           className="message-form"
           onSubmit={e => {
             this.handleSubmit(e);
           }}
         >
-          <FormGroup className="d-flex flex-column">
-            <Row>
-              <Col xs="2">
-                <Button
-                  onClick={e => {
-                    this.handlePrev(e);
-                  }}
-                >
-                  <ArrowLeft />
-                </Button>
-              </Col>
-              <Col xs="8">
-                <Label for="new-message" className="mx-2">
-                  Nouveau Message
-                </Label>
-              </Col>
-            </Row>
+          <FormGroup className="d-flex flex-column py-0 m-0 text-center">
+            <Label for="new-message">Nouveau Message</Label>
             <Input
               type="textarea"
               name="content_form"
               id="content_field"
+              style={{ height: 200 }}
               value={this.state.content_form}
               onChange={e => {
                 this.handleChange(e);
               }}
             />
-            <Button className="my-3 submit-button">Envoyer</Button>
+            <Button className="col-6 mt-4 mx-auto message-btn">Envoyer</Button>
           </FormGroup>
         </Form>
         {this.state.messageArray.length === 0 ? (
@@ -121,45 +132,44 @@ class Message extends React.Component {
         )}
         {this.state.messageArray.map((message, i) => {
           return (
-            <div key={i}>
-              <Card>
-                <CardBody>
-                  <Row>
-                    <Col xs="3">
-                      <img
-                        src={message.avatar}
-                        alt="Avatar"
-                        className="imgAvatar rounded-circle"
-                      />
-                    </Col>
-                    <Col xs="9">
-                      <div className="mx-5 my-3">
-                        <Row>
-                          <Col>
-                            <p className="name">{message.nickname}</p>
-                          </Col>
-                          <Col>
-                            <p className="messageDate timeStamp">
-                              {message.date_diff >= 1
-                                ? "Il y a " + message.date_diff + " jours."
-                                : "Envoyé à " + message.hour_send + "."}
-                            </p>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <p className="bodyText text-justify">
-                            {message.content}
-                          </p>
-                        </Row>
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </div>
+            <Card
+              className={
+                message.id_author === currentUser
+                  ? "message-card myself"
+                  : "message-card"
+              }
+              key={i}
+            >
+              <CardBody>
+                <Row className="message-text">
+                  <Col xs="3" className="d-flex align-items-center">
+                    <img
+                      src={message.avatar}
+                      alt="Avatar"
+                      className="imgAvatar avatar rounded-circle"
+                    />
+                  </Col>
+                  <Col xs="9" className="px-5 my-3">
+                    <Row className="align-items-center">
+                      <Col xs="6">
+                        <p className="name">{message.nickname}</p>
+                      </Col>
+                      <Col xs="6" className="text-right font-italic">
+                        <p className="messageDate timeStamp text-muted">
+                          {message.date_diff >= 1
+                            ? "Il y a " + message.date_diff + " jours."
+                            : "Envoyé à " + message.hour_send + "."}
+                        </p>
+                      </Col>
+                    </Row>
+                    <p className="text-justify">{message.content}</p>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
           );
         })}
-      </div>
+      </Row>
     );
   }
 }
