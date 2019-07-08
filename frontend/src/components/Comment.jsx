5cb8b87472608c7830d5a3cb8b9fd3311b034bd4
Sabine Robart <sabine.robart@protonmail.com>
2019-07-08 14:30:16 +0200

    [Del] the moment.js method since we convert the comments date in the backend request

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import "../style/Comment.scss";
 import ReportButton from "./ReportButton";
 import { Row, Col } from "reactstrap";
-import moment from "moment";
 import { Link } from "react-router-dom";
 
 const Comment = props => {
-  const diff = moment(props.comment.created_at).format("LL");
   return (
     <Row className="px-3 py-1 my-2 align-items-center">
       <Link to={`/profil/${props.profile.id}`}>
@@ -26,7 +24,7 @@ const Comment = props => {
         {props.profile.nickname}
       </Col>
       <Col xs="5" md="5" className="align-items-center comment">
-        <p className="text-muted font-italic m-0">{diff}</p>
+        <p className="text-muted font-italic m-0">{props.timeStamp}</p>
         {props.comment.content}
       </Col>
       <Col xs="1" className="align-items-left">
