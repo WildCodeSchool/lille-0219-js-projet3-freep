80b4dd2d26ca4024843b99e2510b843ee5988639
Louis Allard <l.allard@yahoo.fr>
2019-07-10 15:11:38 +0200

    Erease console.log into reducer.

--- frontend/src/Redux/reducers.js
+++ frontend/src/Redux/reducers.js
@@ -3,10 +3,6 @@ import { SET_RESULTS } from "./actions";
 const searchReducers = (state = [], action) => {
   switch (action.type) {
     case SET_RESULTS:
-      console.log("-----------------");
-      console.log(action.results);
-      console.log("-----------------");
-
       return (state = action.results);
     default:
       return state;
