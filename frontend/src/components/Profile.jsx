986e3de5a12566f3a0691479476ed3e46e35b90f
Richard Gaillet <gaillet.richard@gmail.com>
2019-07-10 17:18:21 +0200

    Change buttons on profile page from Follow & Message to Edit & Log out when profile is authenticated.

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -33,11 +33,12 @@ class Profile extends React.Component {
 
   componentDidMount() {
     const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
-    const currentFollowers = JSON.parse(localStorage.getItem("followers"));
     const user = JSON.parse(localStorage.getItem("user"));
+    const profileId = this.props.match.params.profileId;
 
+    const currentFollowers = JSON.parse(localStorage.getItem("followers"));
     axios
-      .get(`http://localhost:5050/profil/${currentUser}`, {
+      .get(`http://localhost:5050/profil/${profileId}`, {
         headers: {
           Authorization: `Bearer ${user.token}`
         }
@@ -54,17 +55,19 @@ class Profile extends React.Component {
         localStorage.setItem("followers", JSON.stringify(this.state.followers));
       });
 
-    for (let i = 0; i <= currentFollowers.length; i++) {
-      if (currentUser === currentFollowers[i]) {
-        this.setState({
-          isFollowed: true
-        });
+    if (currentFollowers > 0)
+      for (let i = 0; i <= currentFollowers.length; i++) {
+        const followersId = currentFollowers[i].id_user;
+        if (currentUser === followersId) {
+          this.setState({
+            isFollowed: true
+          });
+        }
       }
-    }
   }
 
   handleClick() {
-    const profileId = this.state.user.id;
+    const profileId = this.props.match.params.profileId;
     const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
 
     if (!this.state.isFollowed) {
@@ -94,7 +97,13 @@ class Profile extends React.Component {
     }
   }
 
+  handleLogout() {
+    localStorage.clear();
+    this.props.history.push("/");
+  }
+
   render() {
+    const currentUser = JSON.parse(localStorage.getItem("user")).user.id;
     const user = this.state.user;
     const pictures = this.state.pictures;
     const followers = this.state.followers.length;
@@ -118,7 +127,13 @@ class Profile extends React.Component {
                     </Col>
                     <Col xs="12" className="location">
                       <Map color="#919191" width="18" />
-                      {user.location}
+                      <a
+                        href={`https://google.com/maps/dir//${user.location}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {user.location}
+                      </a>
                     </Col>
                   </Row>
                 </Col>
@@ -157,19 +172,36 @@ class Profile extends React.Component {
                 : 0 + " Post"}
             </Col>
             <Col className="my-5 my-xl-0">
-              <Button
-                onClick={() => {
-                  this.handleClick();
-                }}
-                className="button px-4"
-              >
-                {this.state.isFollowed ? "Suivie ✓" : "Suivre"}
-              </Button>
+              {this.state.user.id !== currentUser ? (
+                <Button
+                  onClick={() => {
+                    this.handleClick();
+                  }}
+                  className="button px-4"
+                >
+                  {this.state.isFollowed ? "Suivie ✓" : "Suivre"}
+                </Button>
+              ) : (
+                <Link to="/modification">
+                  <Button className="button px-4">Éditer</Button>
+                </Link>
+              )}
             </Col>
             <Col className="my-5 my-xl-0">
-              <Link to="/message">
-                <Button className="button px-4">Message</Button>
-              </Link>
+              {this.state.user.id === currentUser ? (
+                <Button
+                  onClick={() => {
+                    this.handleLogout();
+                  }}
+                  className="button px-4"
+                >
+                  Se déconnecter
+                </Button>
+              ) : (
+                <Link to="/message">
+                  <Button className="button px-4">Message</Button>
+                </Link>
+              )}
             </Col>
           </Row>
           <Row>
