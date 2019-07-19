430ff47aefd58b0d76e4f19abd48290c48d20156
Sabine Robart <sabine.robart@protonmail.com>
2019-07-19 23:20:49 +0200

    [Style] fix comments responsiveness

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -9,26 +9,25 @@ const Comment = props => {
   return (
     <Row className="px-3 py-1 my-2 align-items-center">
       <Link to={`/profil/${props.profile.id}`}>
-        <Col md="2" xs="1">
+        <Col xs="2">
           <img
             src={props.profile.avatar}
             alt={`user-${props.profile.id}`}
-            className="comment-avatar avatar"
+            className="comment-avatar"
           />
         </Col>
       </Link>
       <Col
-        md="2"
-        xs="1"
-        className="align-content-center profile-name comments-profile m-0"
+        xs="2"
+        className="align-content-center profile-name comments-profile m-0 p-0"
       >
         {props.profile.nickname}
       </Col>
-      <Col xs="5" md="5" className="align-items-center comment">
+      <Col xs="6" className="align-items-center comment p-0 m-0">
         <p className="text-muted font-italic m-0">{props.timeStamp}</p>
         {props.comment.content}
       </Col>
-      <Col xs="1" className="align-items-left">
+      <Col xs="1" className="align-items-left p-0">
         <ReportButton link={link} />
       </Col>
     </Row>
