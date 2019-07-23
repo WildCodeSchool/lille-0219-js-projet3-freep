169e5a546ce4b47417fbf92308d7e83240eaa863
Sabine Robart <sabine.robart@protonmail.com>
2019-07-23 12:19:31 +0200

    [Fix] issue about the comments users' infos

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom";
 
 const Comment = props => {
   const link = props.comment.id_clothing;
-  const currentUser = JSON.parse(localStorage.getItem("user")).user;
+  const profile = props.profile;
   return (
     <Row className="px-3 py-1 my-2 align-items-center">
-      <Link to={`/profil/${currentUser.id}`}>
+      <Link to={`/profil/${profile.id}`}>
         <Col md="2" xs="1">
           <img
-            src={currentUser.avatar}
-            alt={`user-${currentUser.id}`}
+            src={profile.avatar}
+            alt={`user-${profile.id}`}
             className="comment-avatar"
           />
         </Col>
@@ -23,7 +23,7 @@ const Comment = props => {
         xs="1"
         className="align-content-center profile-name comments-profile m-0"
       >
-        {currentUser.nickname}
+        {profile.nickname}
       </Col>
       <Col xs="5" md="5" className="align-items-center comment">
         <p className="text-muted font-italic m-0">{props.timeStamp}</p>
